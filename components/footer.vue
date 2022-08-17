d48c8a2021a018cd555bc06a7fc305747707dd72
RoySykes <royhudson2099@gmail.com>
2022-08-16 21:38:08 -0500

    dev0003

--- components/footer.vue
+++ components/footer.vue
@@ -1,10 +1,10 @@
 <template>
-<div style="padding-top: 30px">
+<div style="padding-top: 1px">
     <footer class="footer">
       <div class="footer-notes">
         <div class="col" style="float:left;width:60%;">
           <div style="margin-bottom:20px;">GitHub Repository<br/><a href="https://github.com/RoyHudson/Bloodies-Landing-Page" target="_blank">GitHub</a></div>
-          <div style="margin-bottom:20px;">Equipo Responsable<br/><a href="" target="_blank">P  rogramacion Grafica Q2 2022</a></div>
+          <div style="margin-bottom:20px;">Equipo Responsable<br/><a href="" target="_blank">Programacion Grafica Q2 2022</a></div>
           <div style="margin-bottom:20px;">Graphics UI<br/><a href="https://antdv.com/components/overview" target="_blank">Ant Design</a></div>
         </div>
         <div class="col" style="float:left;width:40%;">
@@ -16,7 +16,7 @@
               <i class="fa fa-twitter social-icon"></i>
             </a>
           </div>
-          <div style="margin-bottom:20px;">&copy; {{ new Date().getFullYear() }} Nuxt JS  </div>
+          <div style="margin-bottom:20px;">&copy; {{ new Date().getFullYear() }} Nuxt JS </div>
           <div style="margin-bottom:20px;">
             <a href="https://uip.edu.pa/" target="_blank">
               <img style="float:left;margin-right:15px;position:relative;" src="../assets/banner_uip.png" width="400" height="100"/>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
       <Navbar/>
+      <!-- <Card_image/> -->
+      <!-- <Objetives/> -->
       <Card/>
       <Footer/>
   </div>
@@ -10,8 +12,10 @@
 import Navbar from '../components/navbar.vue';
 import Card from '../components/card.vue';
 import Footer from '../components/footer.vue';
+import Objetives from '../components/objetives.vue';
+import Card_image from '../components/card_image.vue';
 export default {
     name: "IndexPage",
-    components: { Navbar, Card, Footer }
+    components: { Navbar, Card, Footer, Objetives, Card_image }
 }
 </script>
