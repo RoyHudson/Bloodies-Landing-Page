8cca79453130b058acbe003bfecc08e6ceef9320
RoySykes <royhudson2099@gmail.com>
2022-08-16 19:55:41 -0500

    Dev001

---
+++ components/footer.vue
@@ -0,0 +1,47 @@
+<template>
+<div style="padding-top: 30px">
+    <footer class="footer">
+      <div class="footer-notes">
+        <div class="col" style="float:left;width:60%;">
+          <div style="margin-bottom:20px;">GitHub Repository<br/><a href="https://github.com/RoyHudson/Bloodies-Landing-Page" target="_blank">GitHub</a></div>
+          <div style="margin-bottom:20px;">Equipo Responsable<br/><a href="" target="_blank">P  rogramacion Grafica Q2 2022</a></div>
+          <div style="margin-bottom:20px;">Graphics UI<br/><a href="https://antdv.com/components/overview" target="_blank">Ant Design</a></div>
+        </div>
+        <div class="col" style="float:left;width:40%;">
+          <div style="margin-bottom:20px;">
+            <a style="margin-right:20px;" href="https://github.com/cosmicjs" target="_blank">
+              <i class="fa fa-github social-icon"></i>
+            </a>
+            <a href="https://twitter.com/cosmic_js" target="_blank">
+              <i class="fa fa-twitter social-icon"></i>
+            </a>
+          </div>
+          <div style="margin-bottom:20px;">&copy; {{ new Date().getFullYear() }} Nuxt JS  </div>
+          <div style="margin-bottom:20px;">
+            <a href="https://uip.edu.pa/" target="_blank">
+              <img style="float:left;margin-right:15px;position:relative;" src="../assets/banner_uip.png" width="400" height="100"/>
+              <span class="powered-by"></span>
+            </a>
+          </div>
+        </div>
+        <div class="clearfix"></div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<style>
+.footer {
+  background-color: #333;
+  color: #fff;
+  font-size: 18px;
+  width: 100%;
+}
+.footer-notes {
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 15px;
+  padding-bottom: 60px;
+  padding-top: 40px;
+}
+</style>
---
+++ components/navbar.vue
@@ -0,0 +1,47 @@
+<template>
+<div style="padding-bottom: 70px">
+  <a-layout>
+    <a-layout-header :style="{ position: 'fixed', zIndex: 1, width: '100%' }">
+      <div class="logo" />
+      <a-menu
+        theme="dark"
+        mode="horizontal"
+        :style="{ lineHeight: '64px' }"
+      >
+        <a-menu-item key="1">Home</a-menu-item>
+        <a-menu-item key="2">Blog</a-menu-item>
+        <a-menu-item key="3">Grupo Sanguineo</a-menu-item>
+        <a-menu-item key="4">Sobre Nosotros</a-menu-item>
+        <a-menu-item key="5">Login In / Sign Up</a-menu-item>
+      </a-menu>
+    </a-layout-header>
+  </a-layout>
+</div>
+</template>
+<script>
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
+  setup() {
+    return {
+      selectedKeys: ref(['2']),
+    };
+  },
+
+});
+</script>
+<style>
+#components-layout-demo-fixed .logo {
+  width: 120px;
+  height: 31px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px 24px 16px 0;
+  float: left;
+}
+.site-layout .site-layout-background {
+  background: #fff;
+}
+
+[data-theme='dark'] .site-layout .site-layout-background {
+  background: #141414;
+}
+</style>
