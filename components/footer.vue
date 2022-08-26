a851c0808e0917b9de07063aa6706863a79ec2cf
ElmerGJ <elmerdaniel1120@hotmail.com>
2022-08-25 21:10:36 -0500

    footer modified

--- components/footer.vue
+++ components/footer.vue
@@ -1,47 +1,71 @@
 <template>
-<div style="padding-top: 1px">
-    <footer class="footer">
-      <div class="footer-notes">
-        <div class="col" style="float:left;width:60%;">
-          <div style="margin-bottom:20px;">GitHub Repository<br/><a href="https://github.com/RoyHudson/Bloodies-Landing-Page" target="_blank">GitHub</a></div>
-          <div style="margin-bottom:20px;">Equipo Responsable<br/><a href="" target="_blank">Programacion Grafica Q2 2022</a></div>
-          <div style="margin-bottom:20px;">Graphics UI<br/><a href="https://antdv.com/components/overview" target="_blank">Ant Design</a></div>
-        </div>
-        <div class="col" style="float:left;width:40%;">
-          <div style="margin-bottom:20px;">
-            <a style="margin-right:20px;" href="https://github.com/cosmicjs" target="_blank">
-              <i class="fa fa-github social-icon"></i>
-            </a>
-            <a href="https://twitter.com/cosmic_js" target="_blank">
-              <i class="fa fa-twitter social-icon"></i>
-            </a>
-          </div>
-          <div style="margin-bottom:20px;">&copy; {{ new Date().getFullYear() }} Nuxt JS </div>
-          <div style="margin-bottom:20px;">
-            <a href="https://uip.edu.pa/" target="_blank">
-              <img style="float:left;margin-right:15px;position:relative;" src="../assets/banner_uip.png" width="400" height="100"/>
-              <span class="powered-by"></span>
-            </a>
-          </div>
-        </div>
-        <div class="clearfix"></div>
+  <div class="footer">
+      <div class="title">
+        Bloodiest {{ new Date().getFullYear()}} - created with
+        <a href="https://nuxtjs.org/">Nuxt JS</a>
       </div>
-    </footer>
-  </div>
+      <div class="social_networks">
+        <div class="to_twitter">
+          <a href="https://www.google.com/" target="_blank">
+            <img :src="require('/assets/fb_logo.png')" alt="Imagen" width="25">
+          </a>
+        </div>
+        <div class="to_instagram">
+          <a href="https://www.google.com/" target="_blank">
+            <img :src="require('/assets/ig_logo.png')" alt="Imagen" width="25">
+          </a>
+        </div>
+        <div class="to_twitter">
+          <a href="https://www.google.com/" target="_blank">
+            <img :src="require('/assets/tw_logo.png')" alt="Imagen" width="25">
+          </a>
+        </div>
+        <div class="to_youtube">
+          <a href="https://www.google.com/" target="_blank">
+            <img :src="require('/assets/yt_logo.png')" alt="Imagen" width="25">
+          </a>
+        </div>
+
+      </div>
+      <div class="tools">
+        <div class="github">
+          <a href="https://github.com/RoyHudson/Bloodies-Landing-Page" target="_blank">
+            Repository <img :src="require('/assets/github_logo.jpg')" height="25px" alt="Github Logo">
+          </a>
+        </div>
+        <div class="ant-design">
+          <a href="" target="_blank" >
+            UI Library <img :src="require('assets/ant_d_logo.png')" height="25px" alt="Ant Design logo">
+          </a>
+        </div>
+      </div>
+    </div>
 </template>
 
 <style>
-.footer {
-  background-color: #333;
-  color: #fff;
-  font-size: 18px;
-  width: 100%;
-}
-.footer-notes {
-  margin: 0 auto;
-  max-width: 960px;
-  padding: 15px;
-  padding-bottom: 60px;
-  padding-top: 40px;
-}
+  .footer {
+    background-color: #333;
+    color: #fff;
+    font-size: 18px;
+    width: 100%;
+  }
+
+  .social_networks{
+    display: grid;
+    place-content: center;
+    grid-template-columns: 35px 35px 35px 35px;
+  }
+
+  .title{
+    padding: 50px 0 10px;
+    display: block;
+    text-align: center;
+  }
+
+  .tools{
+    padding: 50px;
+    display: grid;
+    place-content: center;
+    grid-template-columns: 200px 200px;
+  }
 </style>
