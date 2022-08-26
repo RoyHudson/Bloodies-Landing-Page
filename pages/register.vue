74dd40832465278759e4acf794c802e6ac106b2e
VictorVergara01 <victorjos2001@hotmail.com>
2022-08-26 14:00:25 -0500

    agrego login register

--- pages/register.vue
+++ pages/register.vue
@@ -1,129 +1,164 @@
 <template>
-<div id="register">
-    <Navbar/>
-  <main class="box">
-    <h2>Register</h2>
+
+ <a-layout >
+    <a-layout-header>
+     <Navbar/>
+    </a-layout-header>
+   
+    <a-layout-content class="layoutR" style="padding: 10px 50px">
+    
+    <div class="backgroundR">
+    
     <form>
-        <div class="inputBox">
-            <label for="nombre">Nombre Completo</label>
-            <input type="text" name="nombre" id="nombre" v-model="nombre" placeholder="Escribe tu nombre completo" required/>
-        </div>
+        <h3><strong>SignUp</strong></h3>
 
-        <div class="inputBox">
-            <label for="email">Email</label>
-            <input type="text" name="email" id="email" v-model="email" placeholder="Escribe tu email" required/>
-        </div>
+        <label for="name"><strong>Nombre</strong></label>
+        <input type="text" placeholder="Ingresa tu nombre" v-model.trim="name" id="name">
 
-        <div class="inputBox">
-            <label for="cellphone">Numero de celular</label>
-            <input type="number" name="cellphone" id="cellphone" v-model="cellphone" placeholder="88888888" required/>
-        </div>
-        <div class="inputBox">
-            <label for="password">Contraseña</label>
-            <input type="password" name="password" id="password" v-model="password" placeholder="Escribe tu contraseña"
-                   required/>
-        </div>
-        <div class="inputBox">
-            <label for="confirm_password">Confirmar Contraseña</label>
-            <input type="password" name="password" id="confirm_password" placeholder="Repite tu contraseña"
-                   required/>
-        </div>
-        <button type="submit" name="" style="float: none;">Sign up</button>
-        <nuxt-link to="/login">
-        <a class="button" href="" style="float: none;">Login</a>
-        </nuxt-link>
+        <label for="email"><strong>Email</strong></label>
+        <input type="text" placeholder="Ingresa tu email" v-model.trim="email" id="email">
+
+        <label for="password"><strong>Password</strong></label>
+        <input type="password" placeholder="Ingresa tu contraseña" v-model.trim="password" id="password"> 
     </form>
-</main>
-</div>
+    <a-button class="loginR" @click="onLogin()">SignUp</a-button>
+    <div class="signup">
+      Or
+    <n-link to="/login" style="font-size:medium ">Login</n-link>
+    </div>
+    </div>
+    
+    
+    </a-layout-content>
+    
+  </a-layout>
 
 </template>
 
+
 <script>
-import Navbar from '../components/navbar.vue';
+
+import { createUserApi } from '../lib/service';
+
 export default {
-    name: "registerPage",
-    data() {
-        return {
-            nombre: "",
-            email: "",
-            cellphone: "",
-            password: "",
-            confirm_password: ""
-        };
-    },
-    components: { Navbar }
+
+data(){
+
+return{
+name:'',
+
+email: '',
+
+password: '',
+
+};
+
+},
+
+methods: {
+
+onLogin: async function() {
+
+const data = {
+
+Email:this.email,
+Password: this.password
+
+};
+
+const result =  await createUserApi(data)
+
+console.log('Respuesta de Strapi ==> ', result)
+
+await this.$router.push('/login');
+
 }
+
+}
+
+};
 
 </script>
-
 <style>
 
-.box {
-    background-color: rgba(0, 0, 0, 0.8);
-    border-radius: 10px;
-    box-shadow: 0 15px 25px rgba(0, 0, 0, 0.8);
-    margin: auto auto;
-    padding: 40px;
+.layoutR{
+
+  height: 600px;
+    width: 500px;
+    background-color: rgba(0, 0, 0, 0.7);
     position: absolute;
+    transform: translate(-50%,-50%);
     top: 50%;
     left: 50%;
-    transform: translate(-50%, -50%);
+    border-radius: 10px;
+    backdrop-filter: blur(10px);
+    border: 2px solid rgba(255,255,255,0.1);
+    box-shadow: 0 0 40px rgba(8,7,16,0.6);
+    padding: 50px 35px;
 }
 
-.box h2 {
-    font-family: Arial, Helvetica, sans-serif;
-    font-weight: 550;
-    margin: 0 0 30px 0;
-    padding: 0;
-    color: #fff;
+.backgroundR{
+    padding: 20px;
+    top: 55%;
+    left: 50%;
+    position: absolute;
+    height: 600px;
+    width: 500px;
+    transform: translate(-50%,-50%);
+    
+}
+
+
+form *{
+    font-family: 'Poppins',sans-serif;
+    color: #ffffff;
+    letter-spacing: 0.5px;
+    outline: none;
+    border: none;
+}
+form h3{
+    color: #ffffff;
+    font-size: 32px;
+    font-weight: 500;
+    line-height: 42px;
     text-align: center;
 }
 
-.box .inputBox label {
-    font-family: Arial, Helvetica, sans-serif;
-    font-size: 18px;
-    font-weight: 550;
-    color: #fff;
+label{
+    display: block;
+    margin-top: 30px;
+    font-size: 16px;
+    font-weight: 500;
 }
-
-.box .inputBox input {
-    background: transparent;
-    border: none;
-    border-bottom: 1px solid #fff;
-    color: #fff;
-    font-size: 18px;
-    letter-spacing: 2px;
-    margin-bottom: 30px;
-    outline: none;
-    padding: 10px 0;
+input{
+    display: block;
+    height: 50px;
     width: 100%;
+    background-color: rgba(255,255,255,0.07);
+    border-radius: 3px;
+    padding: 0 10px;
+    margin-top: 8px;
+    font-size: 14px;
+    font-weight: 300;
 }
-
-.box input[type="submit"], .box button[type="submit"], a.button {
-    font-family: Arial, Helvetica, sans-serif;
-    background: #03a9f4;
-    font-size: 11px;
-    border: none;
-    border-radius: 5px;
-    color: #fff;
-    cursor: pointer;
+::placeholder{
+    color: #e5e5e5;
+}
+.loginR{
+    margin-top: 32px;
+    height: 50px;
+    width: 460px;
+    background-color: #3fa7e4;
+    color: #ffffff;
+    font-size: 18px;
     font-weight: 600;
-    padding: 15px 81.3px;
-    letter-spacing: 2px;
-    outline: none;
-    text-transform: uppercase;
-    text-decoration: none;
-    margin: 5px 5px 5px 5px;
-    display: inline-block;
+    border-radius: 5px;
+    cursor: pointer;
+}
+.signup{
+  color: #e5e5e5;
+  margin-top: 10px;
+  margin-left: 180px;
 }
 
-.box input[type="submit"]:hover, .box button[type="submit"]:hover, a.button:hover {
-    opacity: 0.8;
-}
-
-input[type=number]::-webkit-inner-spin-button,
-input[type=number]::-webkit-outer-spin-button {
-  -webkit-appearance: none;
-  margin: 0;
-}
 </style>
